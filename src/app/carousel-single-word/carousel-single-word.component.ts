3f42e06b59f243b6ed6017f415409e045e98ee3b
shourya0003 <shouryagandham@gmail.com>
2024-02-10 13:12:38 -0500

    Update carousel-single-word.component.ts

--- src/app/carousel-single-word/carousel-single-word.component.ts
+++ src/app/carousel-single-word/carousel-single-word.component.ts
@@ -13,23 +13,23 @@ export class CarouselSingleWordComponent {
   data =[
     {
       image:"../assets/Default_apple_3.jpg",
-      words:"A P P L E  Apple".split(" "),
+      words:"A P P L E  apple".split(" "),
     },
     {
       image:"../assets/Default_bird_4.jpg",
-      words:"B I R D  Bird".split(" ")
+      words:"B I R D  bird".split(" ")
     },
     {
       image:"../assets/Default_car_0.jpg",
-      words:"C A R Car".split(" ")
+      words:"C A R car".split(" ")
     },
     {
       image:"../assets/Default_dog_1.jpg",
-      words:"D O G Dog".split(" ")
+      words:"D O G dog".split(" ")
     },
     {
       image:"../assets/elephant.jpg",
-      words:"E L E P H A N T Elephant".split(" ")
+      words:"E L E P H A N T elephant".split(" ")
     }
     ]
 }
